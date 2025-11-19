7f334da941d8bad0e4e4bf0e39d7fcc8249b304e
Laboratory 07 <lab07@lab.mu.edu>
2025-11-19 15:16:50 +0200

    part1

--- lib/screens/expenses_app.dart
+++ lib/screens/expenses_app.dart
@@ -1,4 +1,4 @@
-import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/screens/expense.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesApp extends StatefulWidget {
